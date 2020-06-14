3cd466866949bf16ebcb8130c37dc2c3e9258831
MrShiister <MrShiister@users.noreply.github.com>
2020-06-15 01:07:17 +0800

    Added Server IP

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::any::Any;
 use std::net::IpAddr;
 use async_std::task;
 use public_ip::{dns, ToResolver, Resolution};
+use dns_lookup::lookup_host;
   
-fn check_connection() -> Option<IpAddr> {
+fn get_internet_ip() -> Option<IpAddr> {
     // List of resolvers to try and get an IP address from
     let resolver = dns::OPENDNS_RESOLVER.to_resolver();
     // Attempt to get an IP address and print it
@@ -26,19 +27,45 @@ fn check_connection() -> Option<IpAddr> {
 
 }
 
-fn main() {
-    let ip = check_connection();
+fn get_server_ip(hostname: String) -> Option<Vec<IpAddr>> {
+    let ip: Result<Vec<IpAddr>, std::io::Error> = lookup_host(&hostname);
 
     match ip {
+        Ok(ip) => Some(ip),
+        Err(error) => {
+            println!("{}", error);
+            None
+        },
+    }
+    // let ip: Option<Vec<std::net::IpAddr>> = lookup_host(&hostname);
+}
+
+fn main() {
+    let internet_ip = get_internet_ip();
+
+    match internet_ip {
         None => {
-            println!("Couldn't get IP!");
+            println!("Couldn't get Internet IP!");
             std::process::exit(1);
         },
         Some(_) => (),
     }
 
-    let ip = ip.unwrap();
+    let internet_ip = internet_ip.unwrap();
+    println!("My IP is {}", internet_ip);
 
-    println!("My IP is {}", ip);
+    let hostname = String::from("sg467.nordvpn.com");
+    let server_ip = get_server_ip(hostname);
+
+    match server_ip {
+        None => {
+            println!("Couldn't get Server IP!");
+            std::process::exit(1);
+        },
+        Some(_) => (),
+    }
+
+    let server_ip = server_ip.unwrap();
+    println!("Server IP is {}", &server_ip[0]);
 
 }
