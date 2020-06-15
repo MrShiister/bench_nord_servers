e4888c20e9498f767654fd22d721423c6cb2fa6d
MrShiister <MrShiister@users.noreply.github.com>
2020-06-15 16:18:18 +0800

    Added switching of servers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::any::Any;
 
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr};
 use async_std::task;
 use public_ip::{dns, ToResolver, Resolution};
 use dns_lookup::lookup_host;
+use std::process::Command;
   
-fn get_internet_ip() -> Option<IpAddr> {
+fn resolve_internet_ip() -> Option<IpAddr> {
     // List of resolvers to try and get an IP address from
     let resolver = dns::OPENDNS_RESOLVER.to_resolver();
     // Attempt to get an IP address and print it
@@ -27,88 +28,94 @@ fn get_internet_ip() -> Option<IpAddr> {
 
 }
 
-fn get_server_ip(hostname: &String) -> Option<IpAddr> {
-    let ip: Result<Vec<IpAddr>, std::io::Error> = lookup_host(&hostname);
-
-    match ip {
-        Ok(ip) => Some(ip[0]),
-        Err(error) => {
-            println!("{}", error);
-            None
-        },
+fn resolve_server_ip(servername: &String) -> Option<IpAddr> {
+    // let ip: Result<Vec<IpAddr>, std::io::Error> = lookup_host(&servername);
+    if let Ok(ip) = lookup_host(&servername) {
+        Some(ip[0])
+    } else {
+        eprintln!("Couldn't resolve server IP!");
+        None
     }
 }
 
+fn build_ip_struct(servername: &String) -> Option<IP> {
+    let wrapped;
+
+    if servername.is_empty() {
+        wrapped = resolve_internet_ip();
+    } else {
+        wrapped = resolve_server_ip(&servername);
+    }
+    
+    if let Some(ip) = wrapped {
+        if let IpAddr::V4(ipv4) = ip {
+            Some(IP {
+                ip: ipv4,
+                octets: ipv4.octets(),
+            })
+        } else {
+            println!("Not handling IPv6!");
+            None
+        }
+    } else {
+        println!("Couldn't resolve Internet IP!");
+        None
+    }
+}
+
+struct IP {
+    ip: Ipv4Addr,
+    octets: [u8; 4],
+}
+
+
 fn main() {
 
-    struct IP {
-        ip: IpAddr,
-        octets: [u8; 4],
-    }
+    let myipname = String::from("");
+    let servername = String::from("sg467.nordvpn.com");
 
-    let mut wrapped;
-    let mut ip;
-    let mut octets;
 
-    let hostname = String::from("sg467.nordvpn.com");
-
+    // to replace with server list
     for _i in 1..4 {
 
+        // Change to server
+        Command::new(r#"C:\Program Files (x86)\NordVPN\NordVPN.exe"#)
+                .arg("-c")
+                .args(&["-n", "Singapore #467"])
+                .output()
+                .expect("Failed to execute NordVPN.exe");
+
         // Get Internet IP
+        let internet = build_ip_struct(&myipname);
 
-        wrapped = get_internet_ip();
-
-        match wrapped {
-            None => {
-                println!("Couldn't get Internet IP!");
-                continue
-                //std::process::exit(1);
-            },
-            Some(_) => (),
+        if let None = internet {
+            eprintln!("Failed to get internet IP!");
+            continue
         }
 
-        ip = wrapped.unwrap();
-
-        octets = match ip {
-            IpAddr::V4(ipv4) => ipv4.octets(),
-            _ => {
-                println!("Not handling IPv6!");
-                continue
-            },
-        };
-
-        let internet = IP {
-            ip,
-            octets,
-        };
+        let internet = internet.unwrap();
         println!("    My IP is {}", internet.ip);
 
-
         // Get Server IP
-        wrapped = get_server_ip(&hostname);
+        let server = build_ip_struct(&servername);
+        if let None = server {
+            eprintln!("Failed to get server IP!");
+            continue
+        }
+        let server = server.unwrap();
+        println!("Server IP is {}", server.ip);
 
-        match wrapped {
-            None => {
-                println!("Couldn't get Server IP!");
-                continue
-            },
-            Some(_) => (),
+        if internet.octets[0] != server.octets[0] ||
+            internet.octets[1] != server.octets[1] ||
+            internet.octets[2] != server.octets[2] ||
+            internet.octets[3] - server.octets[3] > 5 {
+            println!("IP Mismatch.");
+            continue
         }
 
-        ip = wrapped.unwrap();
-
-        octets = match ip {
-            IpAddr::V4(ipv4) => ipv4.octets(),
-            _ => {
-                println!("Not handling IPv6!");
-                continue
-            },
-        };
-        let server = IP {
-            ip,
-            octets,
-        };
-        println!("Server IP is {}", server.ip);
+        // Speedtest
+        
+        // Save score
     }
 
 }
